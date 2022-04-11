7ba585ea0dca0ddf4ce460b4420631118dbf3fad
DinaAbuSmyeh <103364891+DinaAbuSmyeh@users.noreply.github.com>
2022-04-11 02:14:25 +0200

    Add files via upload

---
+++ js/evenodd.js
@@ -0,0 +1,17 @@
+
+function EvenOrOdd(number)
+{
+	if(number.value % 2  === 0)
+	{
+		
+		alert(number.value + " is even number");
+	}
+	else
+	{
+		
+		alert(number.value + " is odd number");
+	}		
+	document.getElementById("balls").src="images/pencils_even.jpeg";
+
+}
+	
---
+++ js/validate.js
@@ -0,0 +1,12 @@
+
+function validateUser(user,psw) 
+{
+	if(user.value=="student" && psw.value=="1")
+	{		
+		alert("Welcome and enjoy educating");
+	}
+	else
+	{
+		alert("invalid usernsme or password");
+	}
+}
